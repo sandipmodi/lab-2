e89b166769b848e66e93689544891140efc0da79
sandipmodi <57409950+sandipmodi@users.noreply.github.com>
2025-11-26 13:09:06 -0500

    Add files via upload

---
+++ lab 2/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String city = "Brampton"; // this the code for my city
+        String subject = "math"; // this is the code for my favorite subject
+        String pet = "bob"; // this is the code for my pet name
+        String number = "360"; // this is the code for the number of students we have in our school
+        String school = "NGS"; // this is the code for my school
+        String principal = "Mrs. Grogan"; // this is the code for my principal name
+
+        System.out.println("Hello! I live in " + city); // this is the code to print my city
+
+        System.out.println("My favorite subject is " + subject); // this is the code to print my favorite subject
+
+        System.out.println("I have a dog named" + pet); // this is the code to print my pets name
+
+
+        System.out.println("irMy principal name is " + principal); // this is the code to print my principal
+        System.out.println("My school is " + school); // this is the code to print my school name
+        System.out.println("We have " + number + " students is our school"); // this is the code to print the number of students we have in our school
+
+
+        System.out.println(5 * 5); // this is the code to print what 5 x 5 = to which is 25
+
+    }
+}
